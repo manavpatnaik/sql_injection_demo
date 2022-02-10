c03ffe6acf25375512e69f8aca7c3d3ed764216e
Manav Patnaik <manavpatnaik@hotmail.com>
2022-02-10 08:00:15 +0530

    Basic client and server initialized

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+const PORT = 5000;
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
